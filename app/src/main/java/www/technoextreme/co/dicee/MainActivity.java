4101b99fcbad0c07f69fc1921d0acc950fe9b35a
Marlon Magonjo <Seven@1Seven@2>
2020-02-06 00:11:20 +0200

    Done Randomizing!

--- app/src/main/java/www/technoextreme/co/dicee/MainActivity.java
+++ app/src/main/java/www/technoextreme/co/dicee/MainActivity.java
@@ -2,7 +2,14 @@ package www.technoextreme.co.dicee;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.Image;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,40 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button rollButton;
+        rollButton = (Button) findViewById(R.id.rollButton);
+
+        final ImageView left_Dice, rightDice;
+        left_Dice = (ImageView) findViewById(R.id.imageLeftDice);
+        rightDice = (ImageView) findViewById(R.id.imageRightDice);
+
+        final int[] diceArray = {R.drawable.dice1,
+                R.drawable.dice2,
+                R.drawable.dice3,
+                R.drawable.dice4,
+                R.drawable.dice5,
+                R.drawable.dice6};
+
+        rollButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Log.d("Dicee", "The button has been pressed!!!");
+
+                Random randomNumberGenerator = new Random();
+
+                int number = randomNumberGenerator.nextInt(6);
+
+                Log.d("Dicee", "The number generated is: " + number);
+
+                left_Dice.setImageResource(diceArray[number]);
+
+                number = randomNumberGenerator.nextInt(6);
+
+                rightDice.setImageResource(diceArray[number]);
+            }
+        });
+
     }
 }
